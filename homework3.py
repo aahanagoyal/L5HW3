18b3f04611e912d29b933f587b3dc4ceaf53d634
aahanagoyal <neha108425@gmail.com>
2025-12-30 20:16:39 +0530

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+age= int(input("Enter your age:"))
+if age >10:
+    if age>10 and  age<=30:
+        print("You are  allowed in sportsclub.") 
+    else:
+        print("You are not allowed.")
+else:
+    print("You are too young.")
